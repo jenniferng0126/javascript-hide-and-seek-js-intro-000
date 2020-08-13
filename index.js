0ae78385600a17e30741a326286104c2222bc752
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-13 18:17:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target');
 }
+
+function increaseRankBy(n) {
+  const liVal = document.querySelectorAll('ul.ranked-list li');
+  for (let i=0; i<li.length; i++) {
+    liVal[i].innerHTML = (parseInt(liVal[i].innerText) + n).toString();
+  }
+}
