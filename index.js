b66ef731634a8dd344d124d1c8a532f67c00af5b
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-13 19:01:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const value = document.querySelector('ul.ranked-list li');
+  const value = document.querySelectorAll('ul.ranked-list li');
   for (let i=0; i<value.length; i++) {
     value[i].innerHTML = (parseInt(value[i].innerText) + n).toString();
   }
